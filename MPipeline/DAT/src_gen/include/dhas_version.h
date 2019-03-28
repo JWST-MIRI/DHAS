fe92909f3a76f243e0f021227e6b6bdd2a62459a
Jane Morrison <morrison@as.arizona.edu>
2019-03-28 16:32:36 -0700

    Updates to version number

--- MPipeline/DAT/src_gen/include/dhas_version.h
+++ MPipeline/DAT/src_gen/include/dhas_version.h
@@ -1,6 +1,6 @@
 #ifndef _DHAS_VERSION_H
 #define _DHAS_VERSION_H
 
-const char dhas_version[]="v9.6.0 March 25, 2019)";  // version of DHAS
+const char dhas_version[]="v9.6.1 March 28, 2019)";  // version of DHAS
 
 #endif
