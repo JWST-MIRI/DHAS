6b43c2e7ee8c5b16e8a0b6f599cca7eb1a9a6ca3
jemorrison <morrison@as.arizona.edu>
2021-12-09 11:36:05 -0500

    updated to version 9.8.11

--- MPipeline/DAT/src_gen/include/dhas_version.h
+++ MPipeline/DAT/src_gen/include/dhas_version.h
@@ -1,6 +1,6 @@
 #ifndef _DHAS_VERSION_H
 #define _DHAS_VERSION_H
 
-const char dhas_version[]="v9.8.10 Dec 6, 2021)";  // version of DHAS
+const char dhas_version[]="v9.8.11 Dec 9, 2021)";  // version of DHAS
 
 #endif
